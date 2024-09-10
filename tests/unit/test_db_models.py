2b6604046198c489f9eda3bbf6d9ddca2b0e61f3
Gideon Oba <deyonoba@gmail.com>
2024-09-10 12:36:04 +0100

    test: Add User model unit test cases

---
+++ tests/unit/test_db_models.py
@@ -0,0 +1,57 @@
+"""
+Unittest for database model schemas.
+
+    - Users
+    - ...
+"""
+import unittest
+from app.db.models import User
+from sqlalchemy import Integer, String
+from typing import Dict, Union
+
+
+def check_column(
+    obj, model, column_name, expected_dtype, 
+    nullable=True, length=None
+):
+    column = model.__table__.columns.get(column_name)
+    
+    # Check if the column exists
+    obj.assertIsNotNone(column, f"column: {column_name} is missing")
+    
+    # Check if column type matches the expected type
+    obj.assertIsInstance(
+        column.type, expected_dtype,
+        msg=f"{column_name} should be of type {expected_dtype}"
+    )
+    
+    # Check the nullable property
+    obj.assertEqual(
+        column.nullable, nullable,
+        f"{column_name} nullable should be {nullable}"
+    )
+    
+    # Check String type property length
+    if length is not None:
+        obj.assertEqual(
+            column.type.length, length,
+            f"{column_name} length should be {length}"
+        )
+
+ 
+class TestUserSchema(unittest.TestCase):        
+    def test_table_name(self):
+        self.assertEqual(User.__tablename__, "users")
+    
+    def test_table_attributes(self):
+        check_column(self, User, 'id', Integer, nullable=False)
+        check_column(self, User, 'username', String, nullable=False, length=250)
+        check_column(self, User, 'hashed_password', String, nullable=False, length=250)
+    
+    def test_primary_key(self):
+        primary_keys = [key.name for key in User.__table__.primary_key]
+        self.assertIn('id', primary_keys, 'id should be a primary key')
+
+
+if __name__ == "__main__":
+    unittest.main(verbose=2)
