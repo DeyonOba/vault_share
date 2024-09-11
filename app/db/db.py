2cbbba07e38ed87d7049c91779eff07d78634d2c
Gideon Oba <deyonoba@gmail.com>
2024-09-11 10:59:39 +0100

    feat: Add DB class module
    Method added to the DB class include:
    1. Private instance method _session that creates the database connection
       session.
    2. Instance method add_user that adds a new user to the database.

---
+++ app/db/db.py
@@ -0,0 +1,40 @@
+"""
+DB module
+"""
+from sqlalchemy import create_engine, URL
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from app.db.models import Base, User
+
+
+class DB:
+    """
+    DB class
+    """
+    
+    def __init__(self, database="app.db") -> None:
+        url_object = URL.create("sqlite", database=database)
+        self._engine = create_engine(url_object, echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+        
+    @property
+    def _session(self) -> Session:
+        """
+        Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+    
+    def add_user(self, username: str, hashed_password: str) -> User:
+        """
+        Add user details to database.
+        """
+        user = User(username=username, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+    
