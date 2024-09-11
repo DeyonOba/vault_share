d7006ed26bfe9ac27037533908aa3d653e47ca82
Gideon Oba <deyonoba@gmail.com>
2024-09-11 15:08:29 +0100

    docs(DB): Add docstring and code comment to instance initilization function

--- app/db/db.py
+++ app/db/db.py
@@ -16,12 +16,26 @@ class DB:
         __session: Memoized session object for database transactions.
     """
     
-    def __init__(self, database="app.db") -> None:
-        url_object = URL.create("sqlite", database=database)
-        self._engine = create_engine(url_object, echo=False)
-        Base.metadata.drop_all(self._engine)
-        Base.metadata.create_all(self._engine)
+    def __init__(
+        self, database_url: str = "sqlite:///app.db", echo: bool = False
+    ) -> None:
+        """
+        Initializes the DB class with a database connection.
+        
+        Args:
+            database_url (str): The database connection URL. Defaults to
+            "sqlite:///app.db"
+            echo (bool): If True, SQLAlchemy logs all SQL statements.
+            Defaults to False.
+        """
+        self._engine = create_engine(url_object, echo=echo)
         self.__session = None
+        # TODO: Remove database initialization drop_all from instance
+        # initialization
+        # Drop all is useful in testing, but would delete impotant data
+        # from production database table when initialized
+        # Base.metadata.drop_all(self._engine)
+        # Base.metadata.create_all(self._engine)
         
     @property
     def _session(self) -> Session:
