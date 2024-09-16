bfda81a3e34d93089a1d419ef88f372bb59fc399
Gideon Oba <deyonoba@gmail.com>
2024-09-16 13:06:12 +0100

    tests(TestDBModule): Parameterize add_user method

--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -5,6 +5,7 @@ import unittest
 from unittest.mock import patch, MagicMock
 from app.db.models import User
 from app.db.db import DB
+from parameterized import parameterized
 
 
 class TestDBModule(unittest.TestCase):
@@ -12,20 +13,33 @@ class TestDBModule(unittest.TestCase):
     def setUp(self):
         """Set up a DB instance with a mock database URL."""
         self.db = DB(database_url="sqlite:///:memory:", echo=False)
-        
+    
+    @parameterized.expand([
+        ("Bob_Dylan", "bod_dylan", "awesomeBob", None),
+        ("John Doe", "john_doe", "justGiveMeAcess", None),
+        ("Dev Success", "dev_success", "PassWord", "admin"),
+    ])   
     @patch("app.db.db.DB._session", new_callable=MagicMock)
-    def test_add_user_success(self, mock_session):
+    def test_add_user_success(self, _, username, password, role, mock_session):
         """Test if user is added to database."""
         # Mock the add and commit behavior
         mock_session.add = MagicMock()
         mock_session.commit = MagicMock()
         
-        
-        user = self.db.add_user("bob_dylan", "awesomeBob")
+        if role is None:
+            user = self.db.add_user(username, password, role="user")
+        else:
+            user = self.db.add_user(username, password, role=role)
         
         # Assert that a User object was returned
         self.assertIsInstance(user, User)
-        self.assertEqual(user.username, "bob_dylan")
+        self.assertEqual(user.username, username)
+        self.assertEqual(user.hashed_password, password)
+        
+        if role is None:
+            self.assertEqual(user.role, "user")
+        else:
+            self.assertEqual(user.role, role)
         
         # Assert that the session's add and commit methods were called
         mock_session.add.assert_called_once_with(user)
