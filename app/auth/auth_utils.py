65828d03e30ae1ab570db3045f07d8df00ac199b
Gideon Oba <deyonoba@gmail.com>
2024-09-17 20:39:55 +0100

    feat(auth_utils): add function verify_password

--- app/auth/auth_utils.py
+++ app/auth/auth_utils.py
@@ -30,14 +30,42 @@ def _hash_password(password: str) -> str:
     pwd: bytes = password.encode('utf-8')
 
     hash_pwd: bytes = hashlib.pbkdf2_hmac('sha256', pwd, salt, 100_000)
-    # Add salt to the begining of the hashed password to ensure easy
+    # Add salt to the beginning of the hashed password to ensure easy
     # password decyrption/validation
     return salt.hex() + hash_pwd.hex()
+
+def verify_password(password: str, stored_password: str) -> bool:
+    """
+    Verifies password with that stored in the database.
+
+    Args:
+        password (str): Current password entered by the user
+        stored_password (str): String representation of the hex password
+        stored in the database    
+
+    Returns:
+        bool: True, if entered password matches the stored password, else False
+    """
+    # Extracts the password salt from the actual hashed password
+    salt = bytes.fromhex(stored_password[:32])
+    stored_password = stored_password[32:]
     
+    current_password = hashlib.pbkdf2_hmac(
+        'sha256', password.encode('utf-8'), salt, 100_000
+    ).hex()
+    
+    return stored_password == current_password
+
+
 if __name__ == "__main__":
     print(f"Try generation UUID: {_generate_uuid()}")
     
     password = "BestPwd"
     print(f"{password=}")
     hashed_password = _hash_password(password)
+    print(f"{type(hashed_password)=}")
+    print(f"{len(hashed_password)=}")
     print(f"Try hashing password (hex): <{hashed_password}>")
+    
+    print(f"Verify password: {verify_password('BestPwd', hashed_password)}")
+    print(f"Verify password: {verify_password('WorstPwd', hashed_password)}")
