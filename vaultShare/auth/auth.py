09ecc4bc34402bb20f01cd9b615f8d56706e28f1
Gideon Oba <deyonoba@gmail.com>
2024-09-23 23:21:06 +0100

    feat(Auth): Add register_user method

--- vaultShare/auth/auth.py
+++ vaultShare/auth/auth.py
@@ -2,7 +2,8 @@
 Module contains class for Authentication.
 """
 from .auth_utils import _generate_uuid, verify_password
-from app.db.db import DB
+from vaultShare.db import DB, UserDB, WorkspaceDB
+from vaultShare.db.models import User
 
 
 class Auth:
@@ -14,3 +15,44 @@ class Auth:
     """
     def __init__(self):
         self._db = DB()
+        self._userdb = UserDB()
+        
+    def register_user(self, username: str, email: str, password: str) -> User:
+        """
+        Use the email and password given to register a user.
+
+        Args:
+            username(str): Account username to be registed
+            email(str): Acount user email
+            password(str): Account user password
+
+        Returns:
+            user(User): User object.
+
+        Raises:
+            ValueError: If username or email already exists in db.
+        """
+        try:
+            user = self._userdb.find_user(username=username)
+        except NoResultFound:
+            user = None
+
+        if user:
+            raise ValueError(f"User {username} already exists")
+        
+        try:
+            user = self._userdb.find_user(email=email)
+        except NoResultFound:
+            user = None
+            
+        if user:
+            raise ValueError(f"User {email} already exists")
+
+        hashed_password = _hash_password(password)
+
+        user = self._userdb.add_user(
+            username=username,
+            email=email,
+            hashed_password=hashed_password
+        )
+        return user
