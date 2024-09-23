63bde29726671f2c1fa58e561cd1c4440d1bd897
Gideon Oba <deyonoba@gmail.com>
2024-09-23 14:56:47 +0100

    test(test_db): Update TestDBModule class

--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -15,51 +15,28 @@ class TestDBModule(unittest.TestCase):
         self.db = DB(database_url="sqlite:///:memory:", echo=False)
     
     @parameterized.expand([
-        ("Bob_Dylan", "bob_dylan", "awesomeBob", None),
-        ("John_Doe", "john_doe", "justGiveMeAcess", None),
+        ("Bob_Dylan", "bob_dylan", "awesomeBob", "user"),
+        ("John_Doe", "john_doe", "justGiveMeAcess", "user"),
         ("Dev_Success", "dev_success", "PassWord", "admin"),
     ])   
-    @patch("app.db.db.DB._session", new_callable=MagicMock)
+    @patch("vaultShare.db.DB._session", new_callable=MagicMock)
     def test_add_user_success(self, _, username, password, role, mock_session):
         """Test if user is added to database."""
         # Mock the add and commit behavior
         mock_session.add = MagicMock()
         mock_session.commit = MagicMock()
         
-        if role is None:
-            user = self.db.add_user(username, password, role="user")
-        else:
-            user = self.db.add_user(username, password, role=role)
-        
+        user = self.db.create(
+            User, username=username, 
+            hashed_password=password, role=role
+        )
+
         # Assert that a User object was returned
         self.assertIsInstance(user, User)
         self.assertEqual(user.username, username)
         self.assertEqual(user.hashed_password, password)
-        
-        if role is None:
-            self.assertEqual(user.role, "user")
-        else:
-            self.assertEqual(user.role, role)
+        self.assertEqual(user.role, role)
         
         # Assert that the session's add and commit methods were called
         mock_session.add.assert_called_once_with(user)
         mock_session.commit.assert_called_once()
-
-    @parameterized.expand([
-        ("Bob_Dylan", "bob_dylan", {"password": "StrongPwd", "role": "admin"}),
-        ("Dev_Success", "dev_success", {"role": "user"}),
-    ])
-    @patch("app.db.db.DB._session", new_callable=MagicMock)
-    def test_update_user(self, _, username: str, update_criteron: dict, mock_session):
-        mock_session.commit = MagicMock()
-        self.db.update_user(username, **update_criteron)
-        mock_session.commit.assert_called_once()
-        
-    @parameterized.expand([
-        ("Bob_Dylan", "bob_dylan", "StrongPwd", "user"),
-        ("Dev_Success", "dev_success", "PassWord", "admin")
-    ])
-    def test_find_user_by(self, _, username, password, role):
-        self.db.add_user(username=username, hashed_password=password, role=role)
-        user = self.db.find_user_by(username=username)
-        self.assertEqual(user.username, username)
