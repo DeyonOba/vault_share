6092460eacad76a75a35cf79de82d9dd3fcf5852
Gideon Oba <deyonoba@gmail.com>
2024-09-25 15:30:23 +0100

    feat(exceptions): add new custom exception class UserAlreadyExists

--- vaultShare/exceptions.py
+++ vaultShare/exceptions.py
@@ -4,6 +4,16 @@
 class MissingFieldError(ValueError):
     """Raises error when an important field is missing from a form-data.
     """
-    msg=""
+    msg = ""
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class UserAlreadyExists(ValueError):
+    """
+    Raises error when a registered user already has the given
+    credentials passed during new user registration.
+    """
+    msg = ""
     def __init__(self, msg):
         self.msg = msg
