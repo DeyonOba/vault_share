618ab0e2f9b71fd90772f0c5958d2c2304976d1f
Gideon Oba <deyonoba@gmail.com>
2024-10-30 10:54:33 +0100

    feat(routes.user): Add update_user_details veiw to '/users' routes

--- vaultShare/routes/users.py
+++ vaultShare/routes/users.py
@@ -9,13 +9,13 @@ users_bp = Blueprint('users', __name__)
 
 def process_user_details(user_obj):
     user = {
-        'id': user_obj.id,
-        'username': user_obj.username,
-        'email': user_obj.email,
-        'role': user_obj.role,
-        'created_at': user_obj.created_at,
-        'memory_allocated': user_obj.memory_allocated,
-        'memory_used': user_obj.memory_used
+        'id': user_obj.get('id'),
+        'username': user_obj.get('username'),
+        'email': user_obj.get('email'),
+        'role': user_obj.get('role'),
+        'created_at': user_obj.get('created_at'),
+        'memory_allocated': user_obj.get('memory_allocated'),
+        'memory_used': user_obj.get('memory_used')
     }
     return user
 
@@ -23,16 +23,41 @@ def process_user_details(user_obj):
 def app_users_details():
     limit = request.args.get('limit')
     users_obj = user_db.find_all_users(limit=limit)
-    users = [process_user_details(user) for user in users_obj]
+    users = [process_user_details(user.__dict__) for user in users_obj]
     
     return jsonify(users)
 
 @users_bp.route('/<username>', methods=['GET'])
 def app_user_detail(username: str):
     try:
-        user = process_user_details(user_db.find_user(username=username))
+        obj = user_db.find_user(username=username)
+        user = process_user_details(obj.__dict__)
     except NoResultFound:
         raise NoUserFound(f"No user {username} found.")
-    return process_user_details(user_db.find_user(username=username))
+    return process_user_details(user)
 
-
+@users_bp.route('/<username>', methods=['PUT'])
+def update_user_details(username: str):
+    """
+    Update specified user details
+    
+    Only details like username, and email can be updated using this
+    route
+    """
+    new_username = request.form.get('username')
+    new_email = request.form.get('email')
+    
+    try:
+        user = user_db.find_user(username=username)
+    except NoResultFound:
+        raise NoUserFound(f"No user {username} found.")
+    
+    update_filter = {"username": user.username}
+    fields_updated = 0
+    if new_username:
+        fields_updated += user_db.update_user(update_filter, username=new_username)
+    if new_email:
+        fields_updated += user_db.update_user(update_filter, email=new_email)
+    
+    message = {"message": f"{fields_updated} fields were updated" if fields_updated else "No field has been updated"}  
+    return jsonify(message)
